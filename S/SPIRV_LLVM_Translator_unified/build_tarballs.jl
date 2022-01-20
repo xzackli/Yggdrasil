ec57ec566b07f11e5adf726f3711df10e0dfa88c
Tim Besard <tim.besard@gmail.com>
2022-01-20 11:25:28 +0100

    Rework SPIRV_LLVM_Translator using augmented platforms.

---
+++ S/SPIRV_LLVM_Translator_unified/build_tarballs.jl
@@ -0,0 +1,121 @@
+using BinaryBuilder, Pkg
+using Base.BinaryPlatforms
+
+include("../../fancy_toys.jl")
+
+name = "SPIRV_LLVM_Translator_unified"
+repo = "https://github.com/maleadt/SPIRV-LLVM-Translator.git"
+version = v"0.1"
+
+llvm_versions = [v"11.0.1", v"12.0.1", v"13.0.0"]
+
+# Collection of sources required to build SPIRV_LLVM_Translator
+sources = Dict(
+    v"11.0.1" => [GitSource(repo, "caa4038025861069adf857be8a5214e6857ff182")],
+    v"12.0.1" => [GitSource(repo, "3ed66fc7524d648dec5a71c5e09985d5aff5bd99")],
+    v"13.0.0" => [GitSource(repo, "776d9041e3baf120222e88842a9d441218a3f791")]
+)
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = expand_cxxstring_abis(supported_platforms(; experimental=true))
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd SPIRV-LLVM-Translator
+install_license LICENSE.TXT
+
+CMAKE_FLAGS=()
+
+# Release build for best performance
+CMAKE_FLAGS+=(-DCMAKE_BUILD_TYPE=Release)
+
+# Install things into $prefix
+CMAKE_FLAGS+=(-DCMAKE_INSTALL_PREFIX=${prefix})
+
+# Explicitly use our cmake toolchain file and tell CMake we're cross-compiling
+CMAKE_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN})
+CMAKE_FLAGS+=(-DCMAKE_CROSSCOMPILING:BOOL=ON)
+
+# Tell CMake where LLVM is
+CMAKE_FLAGS+=(-DLLVM_DIR="${prefix}/lib/cmake/llvm")
+
+# Build the library
+CMAKE_FLAGS+=(-DBUILD_SHARED_LIBS=ON)
+
+cmake -B build -S . -GNinja ${CMAKE_FLAGS[@]}
+ninja -C build -j ${nproc} llvm-spirv install
+install -Dm755 build/tools/llvm-spirv/llvm-spirv${exeext} -t ${bindir}
+"""
+
+# The products that we will ensure are always built
+products = Product[
+    LibraryProduct(["libLLVMSPIRVLib", "LLVMSPIRVLib"], :libLLVMSPIRV, dont_dlopen = true),
+    ExecutableProduct("llvm-spirv", :llvm_spirv),
+]
+
+augment_platform_block = """
+    using Base.BinaryPlatforms
+
+    function augment_platform!(platform::Platform)
+        haskey(platform, "llvm_version") && return p
+
+        llvm_version = Base.libllvm_version
+
+        # does our LLVM build use assertions?
+        llvm_assertions = try
+            cglobal((:_ZN4llvm24DisableABIBreakingChecksE, Base.libllvm_path()), Cvoid)
+            false
+        catch
+            true
+        end
+
+        platform["llvm_version"] = if llvm_assertions
+            "\$(llvm_version.major).asserts"
+        else
+            "\$(llvm_version.major)"
+        end
+
+        return platform
+    end"""
+
+# determine exactly which tarballs we should build
+builds = []
+for llvm_version in llvm_versions, llvm_assertions in (false, true)
+    # Dependencies that must be installed before this package can be built
+    llvm_name = llvm_assertions ? "LLVM_full_assert_jll" : "LLVM_full_jll"
+    dependencies = [
+        BuildDependency(PackageSpec(name=llvm_name, version=llvm_version))
+    ]
+
+    for platform in platforms
+        augmented_platform = deepcopy(platform)
+        augmented_platform["llvm_version"] = if llvm_assertions
+            "$(llvm_version.major).asserts"
+        else
+            "$(llvm_version.major)"
+        end
+
+        should_build_platform(triplet(augmented_platform)) || continue
+        push!(builds, (;
+            dependencies,
+            sources=sources[llvm_version],
+            platforms=[augmented_platform],
+        ))
+    end
+end
+
+# don't allow `build_tarballs` to override platform selection based on ARGS.
+# we handle that ourselves by calling `should_build_platform`
+non_platform_ARGS = filter(arg -> startswith(arg, "--"), ARGS)
+
+# `--register` should only be passed to the latest `build_tarballs` invocation
+non_reg_ARGS = filter(arg -> arg != "--register", non_platform_ARGS)
+
+for (i,build) in enumerate(builds)
+    build_tarballs(i == lastindex(builds) ? non_platform_ARGS : non_reg_ARGS,
+                   name, version, build.sources, script,
+                   build.platforms, products, build.dependencies;
+                   preferred_gcc_version=v"7", julia_compat="1.6",
+                   augment_platform_block)
+end
